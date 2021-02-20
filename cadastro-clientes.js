847361322debd58fd74fd8c7ea81e3e5cf95d23c
Salatiel Aizza <salatiel.aizza@gmail.com>
2021-02-20 00:51:16 +0000

    adicionada página de cadastro de cliente e regra de verificação de cpf

--- cadastro-clientes.js
+++ cadastro-clientes.js
@@ -3,8 +3,12 @@ const formCadastroCliente = document.querySelector('[data-form]')
 formCadastroCliente.addEventListener("submit", event => {
     event.preventDefault()
 
-    const nome = event.target.querySelector('[data-nome]')
-    const cpf = event.target.querySelector('[data-cpf]')
-    cadastrarClientes(nome.value, cpf.value)
+    const nome = event.target.querySelector('[data-nome]').value
+    const cpf = event.target.querySelector('[data-cpf]').value
 
+    if (validaCPF(cpf)) {
+        cadastrarClientes(nome, cpf)
+    } else{
+        alert ('CPF inválido')
+    }
 })
